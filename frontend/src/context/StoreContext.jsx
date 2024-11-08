a2767854a290d999d5f01e8b7deb29b1ceed8f7d
kranthi kumar dande <139384937+kranthikeya@users.noreply.github.com>
2024-11-08 17:49:43 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] =  useState({});
 
-    const url = "https://food-del-rho-eight.vercel.app"
+    const url = "http://food-del-ae3z-backend.vercel.app";
     const [token,setToken] = useState("")
 
     const [food_list,setFoodList] = useState([]);
